1e864768fb0989af54188ae51ef35c157cb4ca36
Eugene Mamaev <mamaeves@mail.ru>
2024-03-21 12:51:54 +0300

    Update provider.php

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -24,8 +24,6 @@
 
 namespace local_accountdate_profile\privacy;
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Privacy Subsystem for local_accountdate_profile implementing null_provider.
  *
@@ -40,7 +38,7 @@ class provider implements \core_privacy\local\metadata\null_provider {
      *
      * @return  string
      */
-    public static function get_reason() : string {
+    public static function get_reason():string {
         return 'privacy:metadata';
     }
 }
