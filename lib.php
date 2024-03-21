cddb2118dac4cbcf2d30dbc61f462dc3f00e45dc
Eugene Mamaev <mamaeves@mail.ru>
2024-03-21 12:52:25 +0300

    Update lib.php

--- lib.php
+++ lib.php
@@ -22,8 +22,6 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die;
-
 /**
  * To add the category and node information into the my profile page.
  *
